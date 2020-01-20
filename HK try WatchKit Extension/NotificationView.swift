d87a2477d59c17943888bb12bcbe0b08f6640bc4
denys <denyspashkov@yahoo.com>
2020-01-20 21:39:16 +0100

    Initial Commit

---
+++ HK try WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  HK try WatchKit Extension
+//
+//  Created by denys on 20/01/2020.
+//  Copyright © 2020 denys. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
